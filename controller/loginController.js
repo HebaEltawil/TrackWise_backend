1e0867be2ac63c676f9052b9b022cf3c4ab856ea
Maiarhu20 <maiarhussein2014@gmail.com>
2025-01-27 05:03:54 +0200

    make login and jwt

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,3 +1,36 @@
+require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../model/usersModel');
+
+const login = async (req,res) => {
+    const {email, password}= req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'Invalid email or password' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+        
+        //  const token = jwt.sign({ id: user._id, email: user.email }, 'your_jwt_secret', {
+        //     expiresIn: '1h',
+        // });
+
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' } 
+        );
+
+        res.json({ message: 'Login successful!', token });
+    } catch (err) {
+        res.status(500).json({ message: 'Something went wrong. Please try again later.' });
+    }
+    
+};
+
+module.exports = login;
