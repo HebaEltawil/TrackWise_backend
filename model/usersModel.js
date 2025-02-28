b6a242ea3bff80768922fc4d9b93afbd705f2313
heba <hebasaadabd03@gmail.com>
2025-02-28 17:33:49 +0200

    edit phonenumber type

--- model/usersModel.js
+++ model/usersModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     lastName:{type:String , required:true},
     email:{type:String , required:true , unique:true},
     password:{type:String , required:true},
-    phoneNumber:{type:Number , required:true},
+    phoneNumber:{type:String , required:true},
     friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     sentRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], 
     receivedRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
