78e1a89fdfb80b21737dbd1e39e91d2f57a5aea1
Maiarhu20 <maiarhussein2014@gmail.com>
2025-02-28 23:07:24 +0200

    edit in middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const extractEmailFromToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Format: "Bearer <token>"
+    const token = req.headers['authorization'];
+    // const token = authHeader && authHeader.split(' ')[1]; // Format: "Bearer <token>"
 
     if (!token) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
@@ -12,10 +12,7 @@ const extractEmailFromToken = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = decoded.id;
-        req.userEmail = decoded.email; // Attach the email to the request object
-        // req.userFirstName = decoded.firstName;
-        // req.userPhone = decoded.phoneNumber;
-        // req.userLastName = decoded.lastName;
+        req.userEmail = decoded.email; 
         next();
     } catch (error) {
         res.status(400).json({ message: 'Invalid token', error: error.message });
